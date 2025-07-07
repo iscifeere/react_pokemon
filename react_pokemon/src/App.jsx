7113ee2804d0a475f3c00674675c8ccca0d09c8e
Alfonso <alfonso.espinoza19@gmail.com>
2025-07-07 13:00:42 -0400

    App y pkmn fetcher añadidos

---
+++ react_pokemon/src/App.jsx
@@ -0,0 +1,12 @@
+import PokemonFetcher from './PokemonFetcher.jsx';
+
+function App() {
+  return (
+    <>
+      <h1>¡Conoce a tus Pokémon!</h1>
+      <PokemonFetcher />
+    </>
+  )
+}
+
+export default App;
